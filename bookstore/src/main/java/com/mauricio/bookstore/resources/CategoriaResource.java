cc3015699f28ced0590f31a2cdc95d23168a9d2d
Mauricio de Assis <mauriciox8mm@hotmail.com>
2021-12-04 18:47:17 -0300

    post para categoria

--- bookstore/src/main/java/com/mauricio/bookstore/resources/CategoriaResource.java
+++ bookstore/src/main/java/com/mauricio/bookstore/resources/CategoriaResource.java
@@ -1,16 +1,22 @@
 package com.mauricio.bookstore.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
+import javax.servlet.Servlet;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.mauricio.bookstore.domain.Categoria;
 import com.mauricio.bookstore.dtos.CategoriaDTO;
@@ -19,32 +25,42 @@ import com.mauricio.bookstore.repository.CategoriaRepository;
 import com.mauricio.bookstore.service.CategoriaService;
 
 @RestController
-@RequestMapping(value ="/v1/categorias")
+@RequestMapping(value = "/v1/categorias")
 public class CategoriaResource {
-	
+
 	/*
-	 *  @Autowired é destinado para realizar a injeção de dependencias 
+	 * @Autowired é destinado para realizar a injeção de dependencias
 	 */
-	@Autowired 
+	@Autowired
 	private CategoriaService service;
-	
-	@GetMapping(value="/{id}") 
+
+	@GetMapping(value = "/{id}")
 	public ResponseEntity<Categoria> findById(@PathVariable Long id) {
-		// passa o ID para o service para realizar a busca do ID 
+		// passa o ID para o service para realizar a busca do ID
 		Categoria obj = service.findById(id);
-		
+
 		return ResponseEntity.ok().body(obj);
 	}
+
 	@GetMapping
-	public ResponseEntity<List<CategoriaDTO>> findAll(){
-		// busca no service todos os resultados 
-		List<Categoria> list= service.findAll();
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		// busca no service todos os resultados
+		List<Categoria> list = service.findAll();
 		/*
-		 * A função do DTO é obter uma lista e adicionar só os atributos desejado, neste caso vamos eliminar a lista de livros para 
-		 * não sobrecarregar o cliente 
+		 * A função do DTO é obter uma lista e adicionar só os atributos desejado, neste
+		 * caso vamos eliminar a lista de livros para não sobrecarregar o cliente
 		 */
-		List<CategoriaDTO> listDTO = list.stream().map(obj ->new CategoriaDTO(obj)).collect(Collectors.toList());
-		return ResponseEntity.ok().body(listDTO) ;
+		List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 	}
-	
+
+	@PostMapping
+	public ResponseEntity<Categoria> create(@RequestBody Categoria obj) {
+		obj = service.create(obj);
+
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
+
+	}
 }
--- bookstore/src/main/java/com/mauricio/bookstore/service/CategoriaService.java
+++ bookstore/src/main/java/com/mauricio/bookstore/service/CategoriaService.java
@@ -33,4 +33,8 @@ public class CategoriaService {
 	public List<Categoria> findAll() {
 		return repository.findAll();
 	}
+	public Categoria create(Categoria obj) {
+		obj.setId(null);
+		return repository.save(obj);
+	}
 }
