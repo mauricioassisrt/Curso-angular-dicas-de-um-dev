a322677cd42605b33d7d72af9a61d6fb9a254583
Mauricio de Assis <mauriciox8mm@hotmail.com>
2021-12-04 11:42:52 -0300

    primeiro comit,

---
+++ bookstore/src/test/java/com/mauricio/bookstore/BookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mauricio.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
