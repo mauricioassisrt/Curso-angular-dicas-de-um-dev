b392996e5f9734bf35f8460d55cb9345e3b6fe6a
Mauricio de Assis <mauriciox8mm@hotmail.com>
2021-12-05 10:37:18 -0300

    delete

--- bookstore/src/main/java/com/mauricio/bookstore/service/LivroService.java
+++ bookstore/src/main/java/com/mauricio/bookstore/service/LivroService.java
@@ -13,38 +13,50 @@ import com.mauricio.bookstore.service.excepitons.ObjectNotFoundException;
 
 @Service
 public class LivroService {
-	
+
 	@Autowired
 	private LivroRepository repository;
 	@Autowired
 	private CategoriaService categoriaService;
+
 	public Livro findById(Long id) {
 		Optional<Livro> obj = repository.findById(id);
-		return obj.orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado "+id+",Tipo "+Livro.class.getName()));
+		return obj.orElseThrow(
+				() -> new ObjectNotFoundException("Objeto não encontrado " + id + ",Tipo " + Livro.class.getName()));
 	}
+
 	public List<Livro> findAll() {
-		
+
 		return repository.findAll();
 	}
+
 	public Livro update(Long id, Livro obj) {
 		Livro newObj = findById(id);
 		updateData(newObj, obj);
-		
+
 		return repository.save(newObj);
 	}
+
 	private void updateData(Livro newObj, Livro obj) {
-		
+
 		newObj.setTitulo(obj.getTitulo());
 		newObj.setNomeAutor(obj.getNomeAutor());
 		newObj.setTexto(obj.getTexto());
-		
+
 	}
+
 	public Livro create(Long id_cat, Livro obj) {
 		obj.setId(null);
 		Categoria cat = categoriaService.findById(id_cat);
-		
+
 		obj.setCategoria(cat);
-		
+
 		return repository.save(obj);
+	}
+
+	public void delete(Long id) {
+		Livro obj = findById(id);
+
+		repository.delete(obj);
 	}
 }
