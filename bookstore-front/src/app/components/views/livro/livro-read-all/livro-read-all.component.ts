9db7f90e64e5150df64b254251353aa779f744de
Mauricio de Assis <mauriciox8mm@hotmail.com>
2021-12-05 23:22:56 -0300

    component livro criado

---
+++ bookstore-front/src/app/components/views/livro/livro-read-all/livro-read-all.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-livro-read-all',
+  templateUrl: './livro-read-all.component.html',
+  styleUrls: ['./livro-read-all.component.css']
+})
+export class LivroReadAllComponent implements OnInit {
+  displayedColumns:string[]=['id', 'titulo', 'livros', 'action']
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
